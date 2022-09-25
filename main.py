1cd8236fe845e98d280a0c5e39d516295004a1a3
Ricardo Micale <ricardomicale@gmail.com>
2022-09-25 16:20:16 -0400

    algoritmo principal listo

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from random import randint
 
 STANDARD_BET: int = 10
-NUMBER_OF_RUNS: int = 5
+NUMBER_OF_RUNS: int = 50
 STARTING_MONEY: int = 30
 GOAL: int = 50
+log: list = []
 
 def updateBet(oldBet: int, winStatus: bool) -> int:
-  newBet: int = oldBet if winStatus else oldBet*2
+  newBet: int = 10 if winStatus else oldBet*2
   return newBet
 
 def updateWinningMonney(winStatus: bool, currentMoney: int, bet: int) -> int:
@@ -20,20 +21,19 @@ def run() -> any:
   currentMoney: int = STARTING_MONEY
   runNumber: int = 0
   winStatus: bool = False
-  log: list = []
 
   while runNumber != NUMBER_OF_RUNS:
+    print(currentMoney, runNumber, bet)
     #bet update
-    if randint(0, 1) >= 0.5:
-      winStatus = True
-      bet = updateBet(bet, winStatus)
-    else:
-      winStatus = False
-      bet = updateBet(bet, winStatus)
+    winStatus = True if randint(0, 1) >= 0.5 else False
+    # if randint(0, 1) >= 0.5:
+    #   winStatus = True
+    # else:
+    #   winStatus = False
     
     #money update
     currentMoney = updateWinningMonney(winStatus, currentMoney, bet)
-    print(currentMoney)
+    bet = updateBet(bet, winStatus)
     #winning conditions
     if currentMoney <= 0:
       runNumber += 1
